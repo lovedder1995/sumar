6c9126a820287319b47aca052e707061ba6156a4
Edder Alberto López Villastrigo <lovedder1995@outlook.com>
2023-12-14 23:59:36 -0600

    Adaptado para la función module

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-return (function () {
-  return function(...$numeros) {
-    return array_reduce($numeros, function ($total, $numero) {
-      $total += $numero;
-      return $total;
-    });
-  };
-})();
+// github:lovedder1995/sumar#2.0.0
+
+return function(...$numeros) {
+  return array_reduce($numeros, function ($total, $numero) {
+    $total += $numero;
+    return $total;
+  });
+};
